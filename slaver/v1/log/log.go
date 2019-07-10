817531205cff5bc7e43f1abddf48c91e883bd1e7
nian.zhou <nian.zhou@office.feng.com>
2019-07-10 13:55:53 +0800

    修改LogToKafka

--- slaver/v1/log/log.go
+++ slaver/v1/log/log.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"sea_log/common"
 	"sea_log/common/sealog_errors"
@@ -12,30 +13,18 @@ func Mapping(perfix string, app *gin.Engine) {
 	log.POST("/inses", sealog_errors.MiddlewareError(LogToKafka))
 }
 
-type Data struct {
-	Topic      string `form:"topic" json:"topic" binding:"required"`
-	Logs       []interface{} `form:"logs" json:"logs" binding:"required"`
-}
-
+//将日志写入kafka
 func LogToKafka(ctx *gin.Context) error {
-	defer func() {
-		if err := recover(); err != nil {
-			return
-		}
-	}()
-	var (
-	data Data
-	err error
-	kafkaLogs  []string
-	)
-	if err = ctx.ShouldBind(&data); err != nil{
-		return err
+	topic := ctx.PostForm("topic")
+	logs := ctx.PostFormArray("logs")
+	if topic == "" || len(logs) == 0 {
+		return errors.New("params_error")
 	}
-	kafkaLogs = make([]string, 0)
-	for _, v := range data.Logs	{
-		kafkaLogs = append(kafkaLogs, v.(string))
+	kafkaLogs := make([]string, 0)
+	for _, log := range logs {
+		kafkaLogs = append(kafkaLogs, log)
 	}
-	if err = kafka.SendToKafka(kafkaLogs, data.Topic); err != nil{
+	if err := kafka.SendToKafka(kafkaLogs, topic); err != nil {
 		return err
 	}
 	ctx.JSON(common.Success())
